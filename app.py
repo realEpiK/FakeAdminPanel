1635815a8197d49d4cd40861392b077393ae8dde
Clayton <claytonreynolds213@gmail.com>
2023-06-29 03:59:43 -0400

    Fixed and added some code C:

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import time
+
 admin_username = "EpiK"
 admin_password = "password"
 
@@ -35,16 +37,34 @@ def main():
 
 def users():
     userList = open("userlist.txt","r")
-    print(userList.readlines())
-
-def banList():
-    print("Ban list")
+    
+    for user in userList:
+        print(user)
+        
+    userList.close()
+    
+def banList(): ## FIXED WITH GPT :)
+    banlist_response = input("Would you like to view the ban list? Y/N:")
+    if banlist_response.lower() == "y": ## FIXED INSTEAD OF USING Y OR y
+        banListFile = open("banlist.txt", "r")
+        for user in banListFile:
+            print(user)
+        banListFile.close()
+    else:  ## INSTEAD OF VIEWING FILE AND CLOSING, MAKE IT ASK IF YOU WANNA VIEW IT AGAIN OR GO BACK TO MENU
+        print("Returning to menu")
+        time.sleep(3)
+        menu()
 
 def kick():
     print("Kick")
 
 def ban():
-    print("Ban")
-
+    banListFile = open("banlist.txt", "a")
+    banUser = input("Ban user: ")
+    banListFile.write(banUser + "\n")  # Write the banned user to the file
+    print(f"You have banned {banUser}!") ## make it return to menu
+    
+    banListFile.close()
+    
 # Call the main function to start the program
 main()
